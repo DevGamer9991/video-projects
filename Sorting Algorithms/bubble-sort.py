a8526d0a4c7243dfec450d54c4e1393948d3c522
DevGamer9991 <pathornton114@gmail.com>
2024-06-05 04:32:13 -0500

    Update bubble-sort.py

--- Sorting Algorithms/bubble-sort.py
+++ Sorting Algorithms/bubble-sort.py
@@ -4,19 +4,11 @@ import numpy as np
 def bubbleSort(arr, i):
     n = len(arr)
 
-    # Last i elements are already in place
     for j in range(0, n-i-1):
-
-        # Traverse the array from 0 to n-i-1
-        # Swap if the element found is greater
-        # than the next element
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
 
-
-# Driver code to test above
 if __name__ == "__main__":
-    # generate a random array of 50 integers from 1 to 100
     arr = np.random.randint(1, 100, 100)
     
     plt.rcParams['toolbar'] = 'None'
@@ -32,16 +24,13 @@ if __name__ == "__main__":
         print(arr)
         
         plt.clf()
-        # plot the array as a bar chart after each iteration and update the plot
+
         plt.bar(range(len(arr)), arr)
         
-        # highlight the sorted part of the array in green
         plt.bar(range(len(arr)-i-1, len(arr)), arr[len(arr)-i-1:], color='g')
-        
-        # highlight the unsorted part of the array in red
+
         plt.bar(range(len(arr)-i-1), arr[:len(arr)-i-1], color='r')
-        
-        # highlight the current element being compared in blue
+
         plt.bar(len(arr)-i-1, arr[len(arr)-i-1], color='b')
         
         plt.tick_params(labelleft=False)
