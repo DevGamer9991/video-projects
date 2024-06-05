00f88925c6d67b9f7ff0a5789c0b1ee52e5ff68f
DevGamer9991 <pathornton114@gmail.com>
2024-06-05 15:54:41 -0500

    Updated selection-sort.py

--- Sorting Algorithms/selection-sort.py
+++ Sorting Algorithms/selection-sort.py
@@ -11,34 +11,25 @@ for i in range(5):
     print(f"Starting in {5-i} seconds...")
     plt.pause(1)
 
-# Traverse through all Aay elements
 for i in range(len(A)-1):
-    
-    # Find the minimum element in remaining 
-    # unsorted Aay
+
     min_idx = i
     for j in range(i+1, len(A)):
         if A[min_idx] > A[j]:
             min_idx = j
-            
-    # Swap the found minimum element with 
-    # the first element        
+      
     A[i], A[min_idx] = A[min_idx], A[i]
     
     print(A)
     
     plt.clf()
     
-    # plot the array as a bar chart after each iteration and update the plot
     plt.bar(range(len(A)), A)
     
-    # highlight the sorted part of the array in green
     plt.bar(range(i+1), A[:i+1], color='g')
-    
-    # highlight the unsorted part of the array in red
+
     plt.bar(range(i+1, len(A)), A[i+1:], color='r')
-    
-    # highlight the current element being compared in blue
+
     plt.bar(i, A[i], color='b')
     
     plt.tick_params(labelleft=False)
@@ -48,7 +39,4 @@ for i in range(len(A)-1):
     
     plt.pause(0.1)
 
-# Driver code to test above
-print ("Sorted Aay")
-for i in range(len(A)):
-    print(A[i],end=" ") 
+plt.show()
